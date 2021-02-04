4c622b6d9ae61582d7420a98651b8f56f47c1459
Yossira98 <yossira.caballero@unah.hn>
2021-02-04 02:29:10 -0600

    Creando frase

--- Ventas/Ventas/Form1.Designer.cs
+++ Ventas/Ventas/Form1.Designer.cs
@@ -28,27 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Ejecutar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Ejecutar
             // 
-            this.button1.Location = new System.Drawing.Point(284, 110);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ejecutar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Ejecutar.BackColor = System.Drawing.SystemColors.Info;
+            this.Ejecutar.Font = new System.Drawing.Font("Lucida Calligraphy", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ejecutar.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.Ejecutar.Location = new System.Drawing.Point(147, 202);
+            this.Ejecutar.Margin = new System.Windows.Forms.Padding(0);
+            this.Ejecutar.Name = "Ejecutar";
+            this.Ejecutar.Size = new System.Drawing.Size(159, 50);
+            this.Ejecutar.TabIndex = 0;
+            this.Ejecutar.Text = "Presiona";
+            this.Ejecutar.UseVisualStyleBackColor = false;
+            this.Ejecutar.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 261);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(395, 261);
+            this.Controls.Add(this.Ejecutar);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Proyecto1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -56,7 +63,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Ejecutar;
     }
 }
 
--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -17,14 +17,14 @@ namespace Ventas
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {       
+            MessageBox.Show("¡¡NO TE DETENGAS HASTA QUE TE SIENTAS ORGULOSO(A)!!");
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Hola Mundo");
         }
     }
 }
